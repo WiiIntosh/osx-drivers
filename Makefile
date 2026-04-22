bb7ff21168cdfe7a4f96959c135679bcb2b3fb36
John Davis <goldfishnet64@outlook.com>
2026-04-22 00:17:32 +0000

    Build: Implement per-version building

--- Makefile
+++ Makefile
@@ -1,25 +1,27 @@
 include common/kext_info.mk
 
-BUILD_PKG	:=	build_pkg
+BUILD_PKG	:=	build_pkg_$(OSX_VERSION)
 KEXTS		:=	WiiAudio WiiEXI WiiGraphics WiiPlatform WiiStorage WiiUSB
-MKEXT_NAME	:=	Wii.mkext
-ARCHIVE_ZIP	:= 	Wiintosh-osx-drivers-$(KEXT_VERSION).zip
+MKEXT_NAME	:=	Wii_$(OSX_VERSION).mkext
+ARCHIVE_ZIP	:= 	Wiintosh-osx-drivers-$(OSX_VERSION)-$(KEXT_VERSION).zip
 
 .PHONY: all clean
 
 all:
+	@echo "OSX target version: $(OSX_VERSION)"
 	$(foreach kext,$(KEXTS),$(MAKE) -C $(kext) all &&) true
 
 	rm -rf $(BUILD_PKG)
 	$(foreach kext,$(KEXTS),mkdir -p $(BUILD_PKG)/Kexts/$(kext).kext &&) true
-	$(foreach kext,$(KEXTS),cp -r $(kext)/build_kext/$(kext).kext $(BUILD_PKG)/Kexts &&) true
+	$(foreach kext,$(KEXTS),cp -r $(kext)/build_kext_$(OSX_VERSION)/$(kext).kext $(BUILD_PKG)/Kexts &&) true
 
-	./make-mkext.py $(BUILD_PKG)/Kexts $(MKEXT_NAME)
-	cp $(MKEXT_NAME) $(BUILD_PKG)
+package:
+	@echo "OSX target version: $(OSX_VERSION)"
+	./make-mkext.py $(BUILD_PKG)/Kexts $(BUILD_PKG)/$(MKEXT_NAME)
 	cd $(BUILD_PKG); zip -qry -FS ../$(ARCHIVE_ZIP) *
 
 clean:
-	rm -rf $(BUILD_PKG)
-	rm -rf $(MKEXT_NAME)
-	rm -rf $(ARCHIVE_ZIP)
+	rm -rf $(BUILD_PKG)*
+	rm -rf *.mkext
+	rm -rf *.zip
 	$(foreach kext,$(KEXTS),$(MAKE) -C $(kext) clean &&) true
--- common/kext.mk
+++ common/kext.mk
@@ -1,41 +1,77 @@
 #
 # Toolchain configuration.
 #
+OSX_VERSIONS_GCC2	:=  cheetah puma
+OSX_VERSIONS_GCC4	:=  jaguar panther tiger
+
+ifeq ($(filter $(OSX_VERSION),$(OSX_VERSIONS_GCC2) $(OSX_VERSIONS_GCC4)),$(OSX_VERSION))
+ifeq ($(filter $(OSX_VERSION),$(OSX_VERSIONS_GCC2)),$(OSX_VERSION))
+# OS X 10.0 and 10.1
+TOOLCHAIN_DIR 	:= 	/usr/bin
+AS 				:= 	$(TOOLCHAIN_DIR)/as
+CC 				:= 	$(TOOLCHAIN_DIR)/gcc2
+CXX 			:= 	$(TOOLCHAIN_DIR)/g++2
+LD 				:= 	$(TOOLCHAIN_DIR)/ld
+else
+# OS X 10.2 or newer
 DARLING_SHELL	:= 	sudo darling shell
 DARLING_CLT 	:= 	/Library/Developer/DarlingCLT/usr/bin
-AS 			:= 	$(DARLING_CLT)/as
-CC 			:= 	$(DARLING_CLT)/powerpc-apple-darwin10-gcc-4.2.1
-CXX 		:= 	$(DARLING_CLT)/powerpc-apple-darwin10-g++-4.2.1
-LD 			:= 	$(DARLING_CLT)/ld_classic
+AS 				:= 	$(DARLING_CLT)/as
+CC 				:= 	$(DARLING_CLT)/powerpc-apple-darwin10-gcc-4.2.1
+CXX 			:= 	$(DARLING_CLT)/powerpc-apple-darwin10-g++-4.2.1
+LD 				:= 	$(DARLING_CLT)/ld_classic
+endif
+else
+$(error Invalid OS X version $(OSX_VERSION))
+endif
 
 #
 # Toolchain flags.
 #
 ASFLAGS	=
 CFLAGS  = 	-fno-builtin -fno-common -mlong-branch -finline -fno-keep-inline-functions
-CFLAGS	+=	-fmessage-length=0  -force_cpusubtype_ALL -static -nostdinc -nostdlib -r
-CFLAGS	+=	-D__KERNEL__ -DKERNEL -DDEBUG -Wall -mmacosx-version-min=10.2 $(INCLUDE)
-CXXFLAGS	=	$(CFLAGS) -x c++ -fapple-kext -fno-rtti -fno-exceptions -fcheck-new
+CFLAGS	+=	-force_cpusubtype_ALL -static -nostdinc -nostdlib -r
+CFLAGS	+=	-D__KERNEL__ -DKERNEL -DDEBUG -Wall $(INCLUDE)
+CXXFLAGS =	$(CFLAGS) -x c++ -fno-rtti -fno-exceptions -fcheck-new
+
+ifeq ($(OSX_VERSION),$(filter $(OSX_VERSION),$(OSX_VERSIONS_GCC2)))
+CXXFLAGS	+=	-fvtable-thunks -findirect-virtual-calls -fpermissive -arch ppc
+else
+CFLAGS		+=	-fmessage-length=0
+CXXFLAGS	+=	-fapple-kext
+endif
+
+ifeq ($(OSX_VERSION),cheetah)
+CFLAGS		-D__MAC_OS_X_VERSION_MIN_REQUIRED=1000
+else ifeq ($(OSX_VERSION),puma)
+CFLAGS		-D__MAC_OS_X_VERSION_MIN_REQUIRED=1010
+else ifeq ($(OSX_VERSION),jaguar)
+CFLAGS		+=	-mmacosx-version-min=10.2
+else ifeq ($(OSX_VERSION),panther)
+CFLAGS		+=	-mmacosx-version-min=10.3
+else ifeq ($(OSX_VERSION),tiger)
+CFLAGS		+=	-mmacosx-version-min=10.4
+endif
 
 #
 # Folders. These must be relative to the kext Makefiles, Darling can't deal with absolute paths.
 #
-BUILD			:=	build
-BUILD_KEXT	:= build_kext
+BUILD		:=	build_$(OSX_VERSION)
+BUILD_KEXT	:=	build_kext_$(OSX_VERSION)
 INCLUDES	:=	../MacPPCKernelSDK/Headers ../include $(SOURCES)
 
 #
 # Source and object files.
 #
 KMOD_CFILE		:= 	$(BUILD)/$(KEXT_NAME)-info.c
-CFILES				:=	$(foreach dir,$(SOURCES),$(wildcard $(dir)/*.c)) $(KMOD_CFILE)
-CXXFILES			:=	$(foreach dir,$(SOURCES),$(wildcard $(dir)/*.cpp))
-OFILES				:=	$(CFILES:%.c=$(BUILD)/%.o) $(CXXFILES:%.cpp=$(BUILD)/%.o)
-KEXT_BIN			:=	$(BUILD)/$(KEXT_NAME)
+CFILES			:=	$(foreach dir,$(SOURCES),$(wildcard $(dir)/*.c)) $(KMOD_CFILE)
+CXXFILES		:=	$(foreach dir,$(SOURCES),$(wildcard $(dir)/*.cpp))
+OFILES			:=	$(CFILES:%.c=$(BUILD)/%.o) $(CXXFILES:%.cpp=$(BUILD)/%.o)
+KEXT_BIN		:=	$(BUILD)/$(KEXT_NAME)
 KEXT_PLIST		:=	$(BUILD)/$(KEXT_NAME)-Info.plist
 KEXT_BUNDLE		:=	$(BUILD_KEXT)/$(KEXT_NAME).kext
 
-INCLUDE				:=	$(foreach dir,$(INCLUDES),-I$(dir))
+INCLUDE			:=	$(foreach dir,$(INCLUDES),-I$(dir))
 DEPENDENCIES 	:= 	$(OFILES:.o=.d)
 
 include ../common/kext_info.mk
@@ -45,8 +81,8 @@ include ../common/kext_info.mk
 all: $(KEXT_BUNDLE)
 
 clean:
-	rm -rf $(BUILD)
-	rm -rf $(BUILD_KEXT)
+	rm -rf $(BUILD)*
+	rm -rf $(BUILD_KEXT)*
 
 # kmod info
 $(KMOD_CFILE): ../common/kmod_info.c
